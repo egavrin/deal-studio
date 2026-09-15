971d7c6b01db51ac8775b400e53e01ff754577aa
Evgeny Gavrin <eugene.a.gavrin@gmail.com>
2026-09-15 21:36:45 +0300

    Repin bounded construction merge

--- app/src/main/java/com/offlineassistant/app/generatedapp/CanonicalDealToolchain.kt
+++ app/src/main/java/com/offlineassistant/app/generatedapp/CanonicalDealToolchain.kt
@@ -347,7 +347,7 @@ internal class CanonicalDealToolchain(
         const val ARTIFACT_SHA256 = "6367bd178fc690132fa92aee813b41b04f102fb58f4aee662790ce526ac11a89"
         const val DEAL_REVISION = "fde98e7bd6e33cc0c41c1a321ab0bffb7b621db2"
         const val DEAL_UI_REVISION = "6b749dc8ace270e19f81987f28a58919572c48a5"
-        const val STREAMING_COMPILER_REVISION = "383309ea6e684d3965c6aaf82c0300772b68217f"
+        const val STREAMING_COMPILER_REVISION = "6ce0525777964acbdc10b5f79dc225c86138219f"
 
         const val ASSET_NAME = "deal-android-toolchain.dex"
         const val PRIOR_V15_ASSET_NAME = "deal-android-toolchain-v15-pr41.dex"
